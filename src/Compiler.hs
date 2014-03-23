ff3c2d70e72a9a5d7857d2a765eb1336aaab30a2
Orvar Segerström <OrvarSegerstrom@gmail.com>
2014-03-23 21:59:45 +0100

    Main now lexes, parses and typechecks

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -1,3 +1,28 @@
-import Grammar.Abs
+import System.Environment
+import System.IO
 
-main = print "OK"
+import qualified Grammar.Par as Par 
+import Grammar.ErrM
+import Grammar.Abs
+import TypeChecker
+
+main = do 
+        args <- getArgs
+        case args of
+          [] -> hGetContents stdin >>= compile
+          fs -> mapM_ ((compile =<<) . readFile) fs
+
+compile :: String -> IO ()
+compile source = do
+    let tree = check source
+    case tree of
+      Bad s    -> putStrLn "ERR" >> putStrLn s
+      Ok  tree -> putStrLn "OK" >> print tree
+
+check :: String -> Err Program
+check source = do
+    let tokens = Par.myLexer source
+    tree  <- Par.pProgram tokens
+    tree' <- typecheck tree
+    return tree'
+      
